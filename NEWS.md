62cee717125932b2e2c19220346d896b477f4e42
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-06-04 07:57:16 -0500

    Updated news

--- NEWS.md
+++ NEWS.md
@@ -1,6 +1,14 @@
+# Updates 06/02/2020
+* Added an ArcGIS toolbox to allow the reports to be called from ArcGIS. 
+* Created a Stand Walk Summary tool that calls the Stand Walk Summary report. 
+* Added Beaver Island summary geodatabase to the test folder. This data has been used for development and testing. This summaries were created on 5/11/2020. 
+* Report currently displays only the summary values calculated by the current FMG summary scripts. Missing values will need to reference new fields that are added to the FMG summary scripts. 
+* Is report supposed to be able to run for multiple hierarchical levels or just the stand level?
+
+
 # Updates 05/31/2020
 
 * Developed a draft "Stand Walk Summary" report based on the version provided by OD-MN. 
 * This report has been developed as an rmarkdown document to take advantage of Pandoc's new [`fenced_div`](https://bookdown.org/yihui/rmarkdown-cookbook/custom-blocks.html) capability. This allows the report data to be semantically arranged in the source document while the output display is styled using `.css` for `.html` output and `.tex` for `.pdf` output. 
 * For `.html` output, this report uses the `.css` `grid` framework to define the layout the report. 
-* For `.pdf` output, this report does not currently contain any `.tex` code to style the report. To support `.pdf` output, this would require an entirely seperate codebase in `.tex` format to mimic the current `.css`-based report layout. Therefore during development, it is recommended to only support the `.html` ouput version, at least until development of the report has matured. Once the report format has matured, a `.pdf` version can be developed to prevent unnecessary duplication of effort during development. 
+* For `.pdf` output, this report does not currently contain any `.tex` code to style the report. To support `.pdf` output, this would require an entirely separate codebase in `.tex` format to mimic the current `.css`-based report layout. Therefore during development, it is recommended to only support the `.html` output version, at least until development of the report has matured. Once the report format has matured, a `.pdf` version can be developed to prevent unnecessary duplication of effort during development. 
--- Stand_Walk_Summary.R
+++ Stand_Walk_Summary.R
@@ -4,7 +4,8 @@
 #' forest stands. 
 #' 
 #' @export
-#' @param stand_list         list; A list of FMG forest stand IDs. 
+#' @param stand_vector      vector; A vector of FMG forest stand IDs. 
+#' @param output_dir        character; The output directory.
 #'
 tool_exec <- function(in_params, out_params) {
   # Load utility R functions
@@ -13,23 +14,13 @@ tool_exec <- function(in_params, out_params) {
   fmg_install <- file.path(fmg, "install")
   source(file.path(fmg_install, "FMG_utils.R"))
   # Load required libraries
-  load_packages(c("sp", "dplyr", "purrr", "tibble"))
+  load_packages(c("dplyr", "purrr", "tibble"))
   # Load FluvialGeomorph R packages
   load_fluvgeo_packages()
   
   # gp tool parameters
-  xs_fc              <- in_params[[1]]
-  xs_points_fc       <- in_params[[2]]
-  bankfull_elevation <- in_params[[3]]
-  lead_n             <- in_params[[4]]
-  use_smoothing      <- in_params[[5]]
-  loess_span         <- in_params[[6]]
-  vert_units         <- in_params[[7]]
-  discharge_method   <- unlist(in_params[[8]])
-  discharge_value    <- in_params[[9]]
-  region             <- in_params[[10]]
-  drainage_area      <- in_params[[11]]
-  width_method       <- in_params[[12]]
+  stand_vector              <- in_params[[1]]
+
   
   # Code for testing in RStudio
   # library(sp)
@@ -37,31 +28,26 @@ tool_exec <- function(in_params, out_params) {
   # library(fluvgeo)
   # library(arcgisbinding)
   # arc.check_product()
-  # xs_fc              <- "D:\\Workspace\\EMRRP_Sediment\\Methods\\FluvialGeomorph\\tests\\data\\test.gdb\\xs_200"
-  # xs_points_fc       <- "D:\\Workspace\\EMRRP_Sediment\\Methods\\FluvialGeomorph\\tests\\data\\test.gdb\\xs_200_points"
-  # bankfull_elevation <- 104.5
-  # lead_n             <- 1
-  # use_smoothing      <- TRUE
-  # loess_span         <- 1
-  # vert_units         <- "ft"
-  # discharge_method   <- "regional_curve"
-  # discharge_value    <- NULL
-  # region             <- "Lower Southern Driftless"
-  # drainage_area      <- 41
-  # width_method       <- NULL
-  # in_params <- list(xs_fc, xs_points_fc, bankfull_elevation, lead_n,
-  #                   use_smoothing, loess_span, vert_units,
-  #                   discharge_method, discharge_value, region, 
-  #                   drainage_area, width_method)
+  # stand_vector              <- "D:\\Workspace\\EMRRP_Sediment\\Methods\\FluvialGeomorph\\tests\\data\\test.gdb\\xs_200"
+  # in_params <- list(stand_list)
   
   # Verify parameters
   ## Create list of parameters (named using the parameter names)
-  param_list <- tibble::lst(xs_fc, xs_points_fc, bankfull_elevation, lead_n,
-                            use_smoothing, loess_span, vert_units,
-                            discharge_method, discharge_value, region, 
-                            drainage_area, width_method)
+  param_list <- tibble::lst(stand_vector, dir_name)
   
-
+  # Iterate through stands
+  for (s in stand_vector) {
+    
+  output_file = file.path(output_dir, )
+  
+  # Render the report
+  rmarkdown::render(input = file.path(dir_name, "Stand_Walk_Summary.Rmd"),
+                    output_format = "html_document",
+                    output_options = list(self_contained = TRUE),
+                    params = list(stand_id),
+                    output_file = output_file,
+                    quite = FALSE)
+  }
   
   return(out_params)
 }
