0be17433fc72e3132b58d7db6e29a7dc7a9eeaa3
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-06-05 11:25:49 -0500

    updated Stand_Walk_Summary.R

--- Stand_Walk_Summary.R
+++ Stand_Walk_Summary.R
@@ -1,19 +1,23 @@
 #' @title Create a Stand Walk Summary Report
 #' 
-#' @description  Creates a Stand Walk Summary Report for the input list of 
-#' forest stands. 
+#' @description  Creates a Stand Walk Summary Report for the forest stands in 
+#' the `stand_summary_tbl`. 
 #' 
 #' @export
-#' @param stand_vector           vector; A vector of FMG forest stand IDs. 
+#' @param stand_summary_tbl      .gdb table; A FMG stand summary table. 
 #' @param age_fixed_summary_tbl  .gdb table; A FMG age & fixed plot summary 
 #'                               table.
-#' @param stand_summary_tbl      .gdb table; A FMG stand summary table. 
 #' @param species_summary_tbl    .gdb table; A FMG species summary table.
 #' @param health_summary_tbl     .gdb table; A FMG Health summary table.
 #' @param imp_stand_summary_tbl  .gdb table; A FMG stand importance summary 
 #'                               table.
 #' @param imp_plot_summary_tbl   .gdb table; A FMG plot importance summary 
 #'                               table.
+#'                               
+#' @details  This tool creates a folder named `reports` in the folder where the 
+#' `stand_summary_tbl` is located. A Stand Walk Summary Report is written into 
+#' the `reports` folder for each forest stand in the `stand_summary_tbl`. 
+#'
 #'
 tool_exec <- function(in_params, out_params) {
   # Load utility R functions
@@ -21,36 +25,37 @@ tool_exec <- function(in_params, out_params) {
   fmg_install <- file.path(dir_name, "install")
   source(file.path(fmg_install, "FMG_utils.R"))
   
+  # Set pandoc path
+  set_pandoc()
+  
   # Load required libraries
-  load_packages(c("dplyr", "purrr", "tibble"))
+  load_packages(c("dplyr", "tibble"))
   
   # gp tool parameters
-  stand_vector              <- in_params[[1]]
+  stand_summary_tbl         <- in_params[[1]]  
   age_fixed_summary_tbl     <- in_params[[2]]
-  stand_summary_tbl         <- in_params[[3]]
-  species_summary_tbl       <- in_params[[4]]
-  health_summary_tbl        <- in_params[[5]]
+  species_summary_tbl       <- in_params[[3]]
+  health_summary_tbl        <- in_params[[4]]
+  imp_stand_summary_tbl     <- in_params[[5]]
+  imp_plot_summary_tbl      <- in_params[[6]]
   
   # Code for testing in RStudio
-  library(sf)
-  dir_name                  <- "D:/Workspace/FMG/Stand_Walk_Sheets/FMG_StandWalk"
-  stand_id                  <- c("p14c007u001st02")
-  age_fixed_summary_tbl     <- "D:\\Workspace\\FMG\\Stand_Walk_Sheets\\FMG_StandWalk\\test\\Beaver_Island_Sum.gdb\\Age_Fixed_Sum"
-  stand_summary_tbl         <- "D:\\Workspace\\FMG\\Stand_Walk_Sheets\\FMG_StandWalk\\test\\Beaver_Island_Sum.gdb\\Stand_Sum"
-  species_summary_tbl       <- "D:\\Workspace\\FMG\\Stand_Walk_Sheets\\FMG_StandWalk\\test\\Beaver_Island_Sum.gdb\\Species_Sum"
-  health_summary_tbl        <- "D:\\Workspace\\FMG\\Stand_Walk_Sheets\\FMG_StandWalk\\test\\Beaver_Island_Sum.gdb\\Health_Sum"
-  imp_stand_summary_tbl     <- "D:\\Workspace\\FMG\\Stand_Walk_Sheets\\FMG_StandWalk\\test\\Beaver_Island_Sum.gdb\\Importance_Stand_Sum"
-  imp_plot_summary_tbl      <- "D:\\Workspace\\FMG\\Stand_Walk_Sheets\\FMG_StandWalk\\test\\Beaver_Island_Sum.gdb\\Importance_Sum"
-  in_params <- list(stand_id, age_fixed_summary_tbl, 
-                    stand_summary_tbl, species_summary_tbl, 
-                    health_summary_tbl, 
-                    imp_stand_summary_tbl, imp_plot_summary_tbl)
+  # library(sf)
+  # dir_name                  <- "D:/Workspace/FMG/Stand_Walk_Sheets/FMG_StandWalk"
+  # stand_summary_tbl         <- "D:\\Workspace\\FMG\\Stand_Walk_Sheets\\FMG_StandWalk\\test\\Beaver_Island_Sum.gdb\\Stand_Sum"
+  # age_fixed_summary_tbl     <- "D:\\Workspace\\FMG\\Stand_Walk_Sheets\\FMG_StandWalk\\test\\Beaver_Island_Sum.gdb\\Age_Fixed_Sum"
+  # species_summary_tbl       <- "D:\\Workspace\\FMG\\Stand_Walk_Sheets\\FMG_StandWalk\\test\\Beaver_Island_Sum.gdb\\Species_Sum"
+  # health_summary_tbl        <- "D:\\Workspace\\FMG\\Stand_Walk_Sheets\\FMG_StandWalk\\test\\Beaver_Island_Sum.gdb\\Health_Sum"
+  # imp_stand_summary_tbl     <- "D:\\Workspace\\FMG\\Stand_Walk_Sheets\\FMG_StandWalk\\test\\Beaver_Island_Sum.gdb\\Importance_Stand_Sum"
+  # imp_plot_summary_tbl      <- "D:\\Workspace\\FMG\\Stand_Walk_Sheets\\FMG_StandWalk\\test\\Beaver_Island_Sum.gdb\\Importance_Sum"
+  # in_params <- list(stand_summary_tbl, age_fixed_summary_tbl, 
+  #                   species_summary_tbl, health_summary_tbl, 
+  #                   imp_stand_summary_tbl, imp_plot_summary_tbl)
 
   # Verify parameters
   ## Create list of parameters (named using the parameter names)
-  param_list <- tibble::lst(stand_id, age_fixed_summary_tbl, 
-                            stand_summary_tbl, species_summary_tbl, 
-                            health_summary_tbl, 
+  param_list <- tibble::lst(stand_summary_tbl, age_fixed_summary_tbl, 
+                            species_summary_tbl, health_summary_tbl, 
                             imp_stand_summary_tbl, imp_plot_summary_tbl)
   
   ## Get parameter verification table
@@ -58,12 +63,10 @@ tool_exec <- function(in_params, out_params) {
   print(compare_params(in_params, param_list))
   
   # Create a `reports` folder in the parent folder that holds the geodatabase
-
   parent_dir <- dirname(dirname(age_fixed_summary_tbl))
   report_dir <- file.path(parent_dir, "reports")
   if(!dir.exists(report_dir)) {
     dir.create(report_dir)
-    message("Report folder created.")
   }
   
   # Convert the geodatabase tables to data frames
@@ -81,12 +84,25 @@ tool_exec <- function(in_params, out_params) {
   imp_plot_summary  <- sf::read_sf(dsn = gdb,
                                    layer = basename(imp_plot_summary_tbl))
   
+  # Get a list of stand_ids from the stand_summary_tbl
+  stand_vector <- stand_summary$SITE_NEW
+  
+  # Create a list to store reports
+  report_files <- list()
+  
   # Iterate through stands
-  for (s in stand_id) {
-    # Create the report name
-    report_name <- paste0("Stand_Summary_", s)
+  for (s in stand_vector) {
+    # Set the report name
+    report_name <- paste0("Stand_Summary_", s, ".html")
     output_file <- file.path(report_dir, report_name)
-    report <- file.path(dir_name, "report", "Stand_Walk_Summary_report.Rmd")
+    report_files <- append(report_files, output_file)
+    
+    # Remove the report file if it already exists
+    if (file.exists(output_file)) {
+      file.remove(output_file)
+    }
+    
+    # Set report parameters
     report_params <- list("stand_id" =  s,
                           "age_fixed_summary" = age_fixed_summary,
                           "stand_summary" = stand_summary,
@@ -94,14 +110,28 @@ tool_exec <- function(in_params, out_params) {
                           "health_summary" = health_summary,
                           "imp_stand_summary" = imp_stand_summary,
                           "imp_plot_summary" = imp_plot_summary)
+    report_template <- file.path(dir_name, "report", 
+                                 "Stand_Walk_Summary_report.Rmd")
     
     # Render the report
-    rmarkdown::render(input = report,
+    rmarkdown::render(input = report_template,
                       output_format = "html_document",
                       output_options = list(self_contained = TRUE),
                       params = report_params,
                       output_file = output_file)
   }
+  
+  # Create an `index.html` file with links to reports
+  output_file2 <- file.path(report_dir, "index.html")
+  report_params2 <- list("report_files" = report_files)
+  report_template2 <- file.path(dir_name, "report", 
+                                "Stand_Walk_Summary_list.Rmd")
+  
+  rmarkdown::render(input = report_template2,
+                    output_format = "html_document",
+                    output_options = list(self_contained = TRUE),
+                    params = report_params2,
+                    output_file = output_file2)
   
   return(out_params)
 }
