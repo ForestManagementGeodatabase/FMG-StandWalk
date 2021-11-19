8f8aaecd556f29a48733ce40a44c476778916c57
Michael Dougherty <mikedoc.p.doc.71@gmail.com>
2021-11-19 11:57:24 -0600

    updated add_site_id function

--- install/FMG_utils.R
+++ install/FMG_utils.R
@@ -198,15 +198,20 @@ fix_fmg_id <- function(df) {
 }
 
 
-add_site_id <- function(df) {
-  # Create the `Site_ID` field from the `SITE` field if it already exists
-  if("SID" %in% colnames(df)) {
+#' @title Add ID Field
+#' 
+#' @param df      data frame; An FMG table to add an ID field to. 
+#' @param level   character; The FMG level to create the ID field from. 
+#'
+add_id <- function(df, level) {
+  if(level == "stand") {
     df <- dplyr::mutate(df, Site_ID = SID)
     
-  } else if("SITE" %in% colnames(df)) {
+  } else if(level == "site") {
     df <- dplyr::mutate(df, Site_ID = SITE)
   }
 }
+
 
 #' @title Convert missing data to NA
 #' 
