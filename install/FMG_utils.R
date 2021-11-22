58eca577b682f053533a9715209e3bce25ab5aab
Michael Dougherty <mikedoc.p.doc.71@gmail.com>
2021-11-22 08:36:14 -0600

    created single page report

--- install/FMG_utils.R
+++ install/FMG_utils.R
@@ -233,3 +233,76 @@ set_na <- function(df) {
   df <- dplyr::mutate_all(df, list(~na_if(.,"<Null>")))
   df <- dplyr::mutate_all(df, list(~na_if(.,"<null>")))
 }
+
+#' @title Create Pandoc ATX Header
+#'
+#' @description Allows programmatic creation of a Pandoc ATX-style header
+#'   with attributes. The currently supported set of Pandoc header attributes
+#'   is: explicit identifier, numbered-unnumbered, listed-unlisted.
+#'
+#' @param text         character; Text of the header.
+#' @param level        integer; Level of the header.
+#' @param identifier   character; Text used for specifying an explicit anchor
+#'                       link.
+#' @param number       logical; Should the header be numbered or unnumbered?
+#'                       Default to TRUE.
+#' @param list         logical; Should heading be included in the table
+#'                       of contents? Default to TRUE.
+#'
+#' @details [`pandoc` extension-header_attributes](https://pandoc.org/MANUAL.html#extension-header_attributes)
+#' @return
+#'
+pandoc_header <- function (text,
+                           level = 1,
+                           identifier = NA,
+                           number = TRUE,
+                           list = TRUE) {
+  # Check inputs
+  if (!is.character(text)) { stop("header text must be character")}
+  if (!is.numeric(level)) { stop("level must be numeric") }
+  if (level < 1 & level <= 6) { stop("level must be between 1 and 6") }
+  if (!is.logical(number)) { stop("number must be logical") }
+  if (!is.logical(list)) { stop("list must be logical") }
+  
+  # Add ATX-style markdown header to text of header
+  header <- paste(atx_header(level), text)
+  
+  # Add html anchor target hash to identifier
+  ident_attribute <- ifelse(is.na(identifier), "",
+                            paste0("#", identifier))
+  # Set unnumbered pandoc attribute
+  number_attribute <- ifelse(number, "", ".unnumbered")
+  
+  # Set unlisted attribute
+  list_attribute <- ifelse(list, "", ".unlisted")
+  
+  # Build space-delimited list of header attributes (and remove white space)
+  attributes <- stringr::str_squish(paste(ident_attribute,
+                                          list_attribute))
+  attributes <- stringr::str_squish(paste(attributes,
+                                          number_attribute))
+  attributes <- paste0("{", attributes, "}")
+  
+  # Combine header and attributes
+  header_attributes <- paste(header, attributes)
+  
+  # Add newlines before and after header
+  header_attributes <- sprintf("\n%s\n", header_attributes)
+  
+  # Use `cat` to output header
+  cat(header_attributes)
+}
+
+#' @title ATX-Style Markdown Header
+#'
+#' @description Produces the ATX-style markdown header (i.e., a number of
+#'   hashes) for the input header level.
+#'
+#' @param level    numeric; Integer number of header level.
+#'
+#' @return String of ATX-style markdown header.
+#'
+atx_header <- function (level) {
+  if(level%%1 != 0) { stop("level must be an integer") }
+  paste(rep.int("#", level), collapse = "")
+}
